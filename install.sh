13b2a19ee581e6eee4f19ef173c299e756c5784e
andersonba <andersonba@gmail.com>
2012-04-10 18:41:35 -0300

    MacOSX compatibility added
    Added "curl" to download "scala" on MacOSX.

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+scala_link='http://www.scala-lang.org/downloads/distrib/files/scala-2.9.1-1.tgz'
+
 if [ ! -f scala-2.9.1-1.tgz ]; then
-	wget http://www.scala-lang.org/downloads/distrib/files/scala-2.9.1-1.tgz
+	if [ `uname` = "Darwin" ]; then
+		curl -O $scala_link
+	else
+		wget $scala_link
+	fi
 fi
 
 if [ ! -d scala-2.9.1-1 ]; then
